b72d809b98f8fee6ce223b001b4b9bbc66fc7b5a
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-12 21:33:38 +0800

    membalikkan kata di stack

--- Stack.java
+++ Stack.java
@@ -5,24 +5,56 @@ import java.util.Scanner;
 public class Stack {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); //menambah objek scanner input
-        int data; //variabel luar stack 
-        StackClass toples = new StackClass(); //objek dan class
+        Scanner input = new Scanner(System.in);
+        StackClass toples1 = new StackClass();
+        StackClass toples2 = new StackClass();
+        StackClass toples3 = new StackClass();
+        StackClass space = new StackClass();
+        String data;
+        int n, td=0, tanda; char piring, flags = ' ', tampung;
+        char[] abjad;
 
         System.out.println("<<<< MEMASUKKAN DATA >>>>");
-        for (int i = 0; i < 5; i++){ //menginput data menggunakan loop
-            System.out.print("Maukkan data : ");data = input.nextInt();
-            toples.push(data);
-        }
-        System.out.print("DATA : "); toples.tampil(); //menampilkan data
-        System.out.println();
+        data = "TES TOL";
+        System.out.print("Masukkan kata : "+data);//data = input.nextLine();
+        n = data.length();
+        abjad = data.toCharArray();
 
         System.out.println("\n<<<< MENGELUARKAN DATA >>>>");
-        toples.tampil();
-        for (int i=0;i<4;i++) { //mengeluarkan data menggunakan objek pop dan perulangan (loop)
-            toples.pop();
-            System.out.println();
-            toples.tampil();
+        for (int i = 0; i < n; i++){
+            toples1.push(abjad[i]);
         }
+        System.out.print("Hasil 1 : ");toples1.tampil();
+
+        System.out.println();
+        while (true) {
+            piring = toples1.pop();
+            td++;
+            if (piring == flags){
+                for (int i = 0; i <= 1; i++){
+                    space.push(piring);
+                }
+                tanda=td;
+                break;
+            } else {
+                toples2.push(piring);
+            }
+        }
+
+        while (true) {
+            tanda++;
+            toples3.push(toples1.pop());
+            if (tanda == n){
+                break;
+            }
+        }
+        System.out.print("Hasil 2 : ");
+        toples2.tampil();
+        space.tampil();
+        toples3.tampil();
+        System.out.println();
+
+
+        System.out.println();
     }
 }
--- StackClass.java
+++ StackClass.java
@@ -1,25 +1,24 @@
 package tugas;
 
 public class StackClass {
-    int stack[] = new int[20];//isi Stack harus dalam bentuk Tipe Array
+    char stack[] = new char[2000];//isi Stack harus dalam bentuk Tipe Array
     int top; //deklarasi variabel top
     
-    public void push (int angka) {
+    public void push (char angka) {
         stack[top]=angka;//itu yang tadi dari Classs utama saya ambilki baru saya taro' diposisi paling atas yang disebut top 
         top= top+1;// bertambah mi topnya 12     
     }
-    public int pop () {
-        int data;//sediakan 1 variabel untuk menampung data yang dikeluarkan dari Stack
+    public char pop () {
+        char data;//sediakan 1 variabel untuk menampung data yang dikeluarkan dari Stack
         top--;// kurangi top terlebih dahulu
         data = stack[top];//keluarkan isi top dan masukkan ke dalam variabel data
-        System.out.print("   data yang akan dikeluarkan : "+data);
         return data;
     }
     
     public void tampil() {
-       for(int i = top-1;i>=0;i--) { //input menggunakan looping
-           System.out.print(stack[i]+" ");
-       }
+        for(int i = top-1;i>=0;i--) { //input menggunakan looping
+            System.out.print(stack[i]+" ");
+        }
     }
     
     
