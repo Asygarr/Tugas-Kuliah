bce543766701c7ea87a1b7a9dd05aa52917cc7aa
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-12 21:52:36 +0800

    tugas membalikkan kata stack.java update 2

--- Stack.java
+++ Stack.java
@@ -1,22 +1,19 @@
 package tugas;
 
-import java.util.Scanner;
 
 public class Stack {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         StackClass toples1 = new StackClass();
         StackClass toples2 = new StackClass();
         StackClass toples3 = new StackClass();
-        StackClass space = new StackClass();
         String data;
-        int n, td=0, tanda; char piring, flags = ' ', tampung;
+        int n, td=0, tanda; char piring, flags = ' ';
         char[] abjad;
 
         System.out.println("<<<< MEMASUKKAN DATA >>>>");
-        data = "TES TOL";
-        System.out.print("Masukkan kata : "+data);//data = input.nextLine();
+        data = "BUKU TULIS";
+        System.out.print("Kata : "+data+"\n");
         n = data.length();
         abjad = data.toCharArray();
 
@@ -25,35 +22,24 @@ public class Stack {
             toples1.push(abjad[i]);
         }
         System.out.print("Hasil 1 : ");toples1.tampil();
-
         System.out.println();
+
         while (true) {
             piring = toples1.pop();
             td++;
             if (piring == flags){
-                for (int i = 0; i <= 1; i++){
-                    space.push(piring);
-                }
                 tanda=td;
-                break;
+                for (int i = tanda; i < n; i++){
+                    toples3.push(toples1.pop());
+                } break;
             } else {
                 toples2.push(piring);
             }
         }
 
-        while (true) {
-            tanda++;
-            toples3.push(toples1.pop());
-            if (tanda == n){
-                break;
-            }
-        }
         System.out.print("Hasil 2 : ");
         toples2.tampil();
-        space.tampil();
         toples3.tampil();
-        System.out.println();
-
 
         System.out.println();
     }
