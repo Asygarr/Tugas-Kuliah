c7a1ea6a8830f3f24bd2c300ebe36693a057115b
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-13 13:08:22 +0800

    tugas membalikkan kata stack.java update 8

--- Stack.java
+++ Stack.java
@@ -13,7 +13,7 @@ public class Stack {
         char piring1, piring2, piring3, flags = ' '; char[] huruf;
 
         System.out.println("<<<< MEMASUKKAN DATA >>>>");
-        data = "BUKU TULIS YUI";
+        data = "BUKU TULIS";
         System.out.print("Kata : "+data+"\n");
         n = data.length();
         huruf = data.toCharArray();
@@ -23,56 +23,27 @@ public class Stack {
             toples1.push(huruf[i]);
         }
         System.out.print("Hasil 1 : ");toples1.tampil();
+        System.out.println();
 
         System.out.print("Hasil 2 : ");
-        while (true) {
-            tanda+=1; //Parameter untuk menghentikan looping
-            piring1 = toples1.pop(); //Penampung data yang dikeluarkan
-            if (tanda == 1){
-                toples2.push(flags); //Mengisi space terlebih dahulu pada pengeluara pertama
-            } if (piring1 == flags){ //Tanda pergantian push toples
-                toples2.tampil(); //Output : YUI
-                toples3.push(piring1); //Mengisi space terlebih dahulu
-                while (true){
-                    tanda+=1; //Parameter untuk menghentikan looping
-                    piring2 = toples1.pop(); //Penampung data yang dikeluarkan
-                    if (piring2 == flags) { //Tanda pergantian push toples
-                        toples3.tampil(); //Output : TULIS
-                        toples4.push(piring2); //Mengisi space terlebih dahulu
-                        while (true) {
-                            tanda+=1; //Parameter untuk menghentikan looping
-                            piring3 = toples1.pop(); //Penampung data yang dikeluarkan
-                            toples4.push(piring3); //Push
-                            if (tanda == n) { //Menghentikan semua looping
-                                toples4.tampil(); //Output : BUKU
-                                break;
-                            }
-                        }
-                        break;
-                    } else {
-                        toples3.push(piring2); //Push
-                    }
-                }
+        for (int i = 0; i < n; i++){
+            piring1 = toples1.pop();
+            if (i == 0) {
+                toples2.push(flags);
+            }if (piring1 == flags) {
+                toples2.tampil();
+                toples3.push(piring1);
+                for (int j = i+1; j < n; j++){
+                    piring2 = toples1.pop();
+                    toples3.push(piring2);
+                } toples3.tampil();
                 break;
             } else {
-                toples2.push(piring1); //Push
+                toples2.push(piring1);
             }
         }
 
-        /*for (int i = 0; i < n; i++){
-            tanda++;
-            piring = toples1.pop();
-            if (i == 0){
-                toples2.push(flags);
-            } if (piring == flags) {
-                for (int j = tanda; j < n; j++){
-                    toples3.push(toples1.pop());
-                }
-                break;
-            } else {
-                toples2.push(piring);
-            }
-        }*/
+
 
         System.out.println();
     }
