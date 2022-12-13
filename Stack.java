4c1395f528368bdf187b430c9cdff13e10c1f562
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-13 14:16:42 +0800

    tugas membalikkan kata stack.java update MAJOR 2

--- Stack.java
+++ Stack.java
@@ -11,7 +11,7 @@ public class Stack {
         char piring, flags = ' '; char[] huruf;
 
         System.out.println("<<<< MEMASUKKAN DATA >>>>");
-        data = "BUKU TULIS";
+        data = "BUKU TULIS YUI";
         System.out.print("Kata : "+data+"\n");
         n = data.length();
         huruf = data.toCharArray();
@@ -25,32 +25,42 @@ public class Stack {
 
         System.out.print("Hasil 2 : ");
         for (int i = 0; i < n; i++){
+            // Start
             piring = toples1.pop();
-
-            // Mengisi space terlebih dahulu
             if (i == 0) {
                 toples2.push(flags);
             }
 
-            // Penukaran kata
             if (piring == flags) {
-                toples2.tampil(); // Menampilkan kata pertama
-
-                // Menghapus isi di toples 2
+                toples2.tampil();
                 for (int j = i; j >= 0; j--){
                     toples2.pop();
                 }
 
-                // Mengisi kata ke-2 di toples 2
                 toples2.push(piring);
                 for (int j = i+1; j < n; j++){
                     piring = toples1.pop();
-                    toples2.push(piring);
+
+                    if (piring == flags) {
+                        toples2.tampil();
+                        for (int k = j; k >= i+1; k--){
+                            toples2.pop();
+                        }
+
+                        toples2.push(piring);
+                        for (int k = j+1; k < n; k++){
+                            piring = toples1.pop();
+                            toples2.push(piring);
+                        }
+                        toples2.tampil();
+                        break;
+                    } else {
+                        toples2.push(piring);
+                    }
                 }
-                toples2.tampil(); // Menampilkan kata kedua
-                break; // Berhenti loop
+                break;
             } else {
-                toples2.push(piring); // Mengisi kata pertama di toples 2
+                toples2.push(piring);
             }
         }
 
