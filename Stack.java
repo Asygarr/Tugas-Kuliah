f938db5e5be028154bc0fd52ef220b8a8d8bbeeb
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-13 13:44:20 +0800

    tugas membalikkan kata stack.java update MAJOR

--- Stack.java
+++ Stack.java
@@ -6,11 +6,9 @@ public class Stack {
     public static void main(String[] args) {
         StackClass toples1 = new StackClass();
         StackClass toples2 = new StackClass();
-        StackClass toples3 = new StackClass();
-        StackClass toples4 = new StackClass();
         String data;
-        int n, tanda = 0;
-        char piring1, piring2, piring3, flags = ' '; char[] huruf;
+        int n;
+        char piring1, flags = ' '; char[] huruf;
 
         System.out.println("<<<< MEMASUKKAN DATA >>>>");
         data = "BUKU TULIS";
@@ -28,22 +26,33 @@ public class Stack {
         System.out.print("Hasil 2 : ");
         for (int i = 0; i < n; i++){
             piring1 = toples1.pop();
+
+            // Mengisi space terlebih dahulu
             if (i == 0) {
                 toples2.push(flags);
-            }if (piring1 == flags) {
-                toples2.tampil();
-                toples3.push(piring1);
-                for (int j = i+1; j < n; j++){
-                    piring2 = toples1.pop();
-                    toples3.push(piring2);
-                } toples3.tampil();
-                break;
-            } else {
+            }
+
+            // Penukaran kata
+            if (piring1 == flags) {
+                toples2.tampil(); // Menampilkan kata pertama
+
+                // Menghapus isi di toples 2
+                for (int j = i; j >= 0; j--){
+                    toples2.pop();
+                }
+
+                // Mengisi kata ke-2 di toples 2
                 toples2.push(piring1);
+                for (int j = i+1; j < n; j++){
+                    piring1 = toples1.pop();
+                    toples2.push(piring1);
+                }
+                toples2.tampil(); // Menampilkan kata kedua
+                break; // Berhenti loop
+            } else {
+                toples2.push(piring1); // Mengisi kata pertama di toples 2
             }
         }
-
-
 
         System.out.println();
     }
