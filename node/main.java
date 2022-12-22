7ca04befc17fe070de4ea66193186aa23320022c
Asygarr <105841104721@student.unismuh.ac.id>
2022-12-22 13:58:01 +0800

    Tiket ke final STD

---
+++ node/main.java
@@ -0,0 +1,21 @@
+package tugas.node;
+
+public class main {
+
+    public static void main (String[] args){
+        
+        operasi rantai= new operasi();
+
+        rantai.tambah_depan(6);
+        rantai.tambah_belakang(5);
+        rantai.tambah_depan(8);
+        rantai.tambah_belakang(4);
+        rantai.tambah_depan(9);
+
+        // Tugas
+        rantai.tengah_urut(7);
+        
+        rantai.tampil();
+    }
+    
+}
